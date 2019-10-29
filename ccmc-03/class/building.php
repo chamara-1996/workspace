195d0f1ec0d7839b906a8d6030bdcdc97907d803
chamara-1996 <chamaradinushan1100@gmail.com>
2019-10-29 06:13:09 +0000

    first commit

---
+++ ccmc-03/class/building.php
@@ -0,0 +1,31 @@
+<?php
+class Building{
+    //properties
+    private $name;
+    private $height;
+    
+    //constractor
+    function __construct($name, $height){
+        $this->name = $name;
+        $this->height = $height;
+        
+    }
+    //accessor method
+    function setName($name){
+      $this->name = $name;
+    }
+    function getName(){
+        return $this->name;
+    }
+    function setHeight($height){
+      $this->height = $height;
+    }
+    function getHeight(){
+        return $this->height;
+    }
+    function getprofie() {
+        $profile = "{$this->name}建物の名前の高さは{$this->height}ｍです。";
+        return $profile;
+    }
+}
+?>
